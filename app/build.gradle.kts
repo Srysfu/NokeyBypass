a9a5f05a720574cf3588c5826dc1a42b3851c1c3
maodaiyong <maodaiyong@users.noreply.github.com>
2026-07-19 15:09:40 +0000

    Initial commit: NokeyBypass Xposed module - complete buildable project

---
+++ app/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.example.nokeybypass"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.nokeybypass"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    compileOnly("de.robv.android.xposed:api:82")
+    compileOnly("de.robv.android.xposed:api:82:sources")
+}
